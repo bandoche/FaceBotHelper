89e7a67709c73e6ec28c81dc2a5239ded4d2f4aa
Jung Sang-jun <bandoche@hanmail.net>
2013-01-12 16:06:23 +0900

    Initial Commit

---
+++ FaceBotHelper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FaceBotHelper
+//
+//  Created by Sang-jun Jung on 13. 1. 12..
+//  Copyright (c) 2013년 Sang-jun Jung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FaceBotHelper/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  FaceBotHelper
+//
+//  Created by Sang-jun Jung on 13. 1. 12..
+//  Copyright (c) 2013년 Sang-jun Jung. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
